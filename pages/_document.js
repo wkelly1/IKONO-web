271579b7292e5d0dc289b79a9bb0a93056771402
Will Kelly <william.kelly20@btinternet.com>
2021-09-07 23:53:20 +0100

    Moving meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -22,33 +22,6 @@ class MyDocument extends Document {
           />
           <link rel="manifest" href="/manifest.json" />
           <title>IKONO</title>
-
-          <meta name="og:url" contant="https://ikono.will-kelly.co.uk" />
-          <meta property="og:type" content="website" />
-          <meta property="og:site_name" content="IKONO" />
-          <meta
-            property="og:description"
-            content="Free SVG icon pack with MIT license."
-          />
-          <meta property="og:title" content="IKONO" />
-          <meta
-            property="og:image"
-            content="https://ikono.will-kelly.co.uk/banner.png"
-          />
-          <meta name="twitter:card" content="app" />
-          <meta name="twitter:site" content="@WillKelly__" />
-          <meta name="twitter:title" content="IKONO" />
-          <meta
-            name="twitter:description"
-            content="Free SVG icon pack with MIT license"
-          />
-          <meta name="twitter:app:name:iphone" content="IKONO" />
-          <meta name="twitter:app:name:ipad" content="IKONO" />
-          <meta name="twitter:app:name:googleplay" content="IKONO" />
-          <meta
-            name="twitter:image"
-            content="https://ikono.will-kelly.co.uk/banner.png"
-          />
         </Head>
         <body>
           <Main />
--- pages/index.js
+++ pages/index.js
@@ -320,6 +320,33 @@ export default function Home() {
           <Head>
             <title>IKONO</title>
             <link rel="icon" href="/favicon.ico" />
+
+            <meta name="og:url" contant="https://ikono.will-kelly.co.uk" />
+            <meta property="og:type" content="website" />
+            <meta property="og:site_name" content="IKONO" />
+            <meta
+              property="og:description"
+              content="Free SVG icon pack with MIT license."
+            />
+            <meta property="og:title" content="IKONO" />
+            <meta
+              property="og:image"
+              content="https://ikono.will-kelly.co.uk/banner.png"
+            />
+            <meta name="twitter:card" content="app" />
+            <meta name="twitter:site" content="@WillKelly__" />
+            <meta name="twitter:title" content="IKONO" />
+            <meta
+              name="twitter:description"
+              content="Free SVG icon pack with MIT license"
+            />
+            <meta name="twitter:app:name:iphone" content="IKONO" />
+            <meta name="twitter:app:name:ipad" content="IKONO" />
+            <meta name="twitter:app:name:googleplay" content="IKONO" />
+            <meta
+              name="twitter:image"
+              content="https://ikono.will-kelly.co.uk/banner.png"
+            />
           </Head>
 
           <header className="flex items-center justify-between px-5 pt-10 sm:px-16 sm:pt-14">
