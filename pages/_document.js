7b2a355140f8dbd9dc24f5eb97a1f20f5f00902f
Will Kelly <william.kelly20@btinternet.com>
2021-09-08 00:03:47 +0100

    Fixing font

--- pages/_document.js
+++ pages/_document.js
@@ -20,6 +20,11 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap"
             rel="stylesheet"
           />
+
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
           <link rel="manifest" href="/manifest.json" />
           <title>IKONO</title>
         </Head>
